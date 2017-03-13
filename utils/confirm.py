c793f3364d3803b7d6d4e0b0e9306d8ea77dd50c
NerdieTony <Nerdietony@users.noreply.github.com>
2017-03-13 07:01:38 -0400

    Update confirm.py

--- utils/confirm.py
+++ utils/confirm.py
@@ -1,9 +1,20 @@
 from discord.ext import commands
+import discord
 
 
-def is_owner_check(ctx):
-    return ctx.message.author.id == "ID"
+class Checks:
+    def __init__(self, bot):
+        self.xeili = xeili 
+
+    def is_owner_check(ctx):
+        ownerid = await self.get_owner_id()
+        return ctx.message.author.id == ownerid
 
 
-def instance_owner():
-    return commands.check(is_owner_check)
+    def instance_owner():
+        return commands.check(is_owner_check)
+
+    async def get_owner_id(self):
+        owner = await self.xeili.application_info()
+        owner = str(owner.id)
+        return owner
