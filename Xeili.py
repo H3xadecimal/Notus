385db1485d853d304047258b59b312c5775a3d0f
martmists <martmists@gmail.com>
2017-05-08 16:06:51 +0000

    Add music

--- Xeili.py
+++ Xeili.py
@@ -1,11 +1,7 @@
-import discord
 from discord.ext import commands
 from utils.dataIO import dataIO
-import asyncio
 import traceback
-import discord.errors
 import redis
-import os
 import argparse
 import json
 
@@ -22,15 +18,22 @@ prefix = config.get('XEILI_PREFIX')
 # CMD-L Arguments
 parser = argparse.ArgumentParser()
 redis_grp = parser.add_argument_group('redis')
-redis_grp.add_argument('--host', type=str, help='the Redis host', default=redis_host)
-redis_grp.add_argument('--port', type=int, help='the Redis port', default=redis_port)
-redis_grp.add_argument('--db', type=int, help='the Redis database', default=redis_db)
-redis_grp.add_argument('--password', type=str, help='the Redis password', default=redis_pass)
+redis_grp.add_argument('--host', type=str,
+                       help='the Redis host', default=redis_host)
+redis_grp.add_argument('--port', type=int,
+                       help='the Redis port', default=redis_port)
+redis_grp.add_argument('--db', type=int,
+                       help='the Redis database', default=redis_db)
+redis_grp.add_argument('--password', type=str,
+                       help='the Redis password', default=redis_pass)
 args = parser.parse_args()
 
 # Redis Connection Attempt... hopefully works.
 try:
-    redis_conn = redis.StrictRedis(host=args.host, port=args.port, db=args.db, password=args.password)
+    redis_conn = redis.StrictRedis(host=args.host,
+                                   port=args.port,
+                                   db=args.db,
+                                   password=args.password)
 except:
     print('aaaaaaa unable to redis 404')
     exit(2)
@@ -52,27 +55,31 @@ class Xeili(commands.Bot):
             pass
 
     async def on_ready(self):
-        self.redis.set('__info__', 'This database is being used by the Xeili Framework.')
+        self.redis.set('__info__',
+                       'This database is being used by the Xeili Framework.')
         print('Ready.')
         print(self.user.name)
 
         self.load_extension('modules.compact')
 
     async def on_command_error(self, exception, context):
-        channel = context.message.channel
+        print(exception)
         if isinstance(exception, commands.errors.CommandNotFound):
             pass
         if isinstance(exception, commands.errors.MissingRequiredArgument):
-            await self.send_cmd_help(context)
+            await send_cmd_help(context)
         elif isinstance(exception, commands.errors.CommandInvokeError):
-             exception = exception.original
-             _traceback = traceback.format_tb(exception.__traceback__)
-             _traceback = ''.join(_traceback)
-             error = '`{0}` in command `{1}`: ```py\nTraceback (most recent call last):\n{2}{0}: {3}\n```'\
-                 .format(type(exception).__name__, context.command.qualified_name, _traceback, exception)
-             await context.send(error)
+            exception = exception.original
+            _traceback = traceback.format_tb(exception.__traceback__)
+            _traceback = ''.join(_traceback)
+            error = ('`{0}` in command `{1}`: ```py\n'
+                     'Traceback (most recent call last):\n{2}{0}: {3}\n```')\
+                .format(type(exception).__name__,
+                        context.command.qualified_name,
+                        _traceback, exception)
+            await context.send(error)
         elif isinstance(exception, commands.errors.CommandNotFound):
-             pass
+            pass
 
     async def on_message(self, message):
         if message.author.bot:
@@ -83,11 +90,11 @@ class Xeili(commands.Bot):
 
 async def send_cmd_help(ctx):
     if ctx.invoked_subcommand:
-        _help = await ctx.bot.formatter.format_help_for(ctx, ctx.invoked_subcommand)
+        _help = await ctx.bot.formatter.format_help_for(
+                ctx, ctx.invoked_subcommand)
     else:
         _help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
     for page in _help:
-        # noinspection PyUnresolvedReferences
         await ctx.send(page)
 
 
---
+++ modules/music.py
@@ -0,0 +1,219 @@
+import asyncio
+import audioop
+import functools
+import time
+
+import discord
+import youtube_dl as ytdl
+from discord.ext import commands
+
+
+def music_after(e):
+    print(e)
+
+async def get_entry(song, opts, loop):
+    with ytdl.YoutubeDL(opts) as dl:
+        f = functools.partial(dl.extract_info, song, download=False)
+
+    res = await loop.run_in_executor(None, f)
+
+    try:
+        res = res['entries'][0]
+    except KeyError:
+        pass
+
+    # Create an entry
+    entry = {
+        'title': res["title"],
+        'uploader': res.get('uploader'),
+        'stream': res.get('is_live', False),
+        'duration': res.get('duration'),
+        'url': res['webpage_url'],
+        'raw_url': res['url'],
+    }
+
+    return entry
+
+
+class OverlaySource(discord.AudioSource):
+    def __init__(self, source, overlay, *, vc):
+        self.source = source
+        self.overlay = overlay
+        self._overlay_source = discord.FFmpegPCMAudio(overlay)
+        self.vc = vc
+        self.vol = 1
+        self.vol_step = .1
+
+    def read(self):
+        source_data = self.source.read()
+        overlay_data = self._overlay_source.read()
+
+        if not source_data:
+            self.vc.source = self._overlay_source
+            self.cleanup()
+            return overlay_data
+
+        source_data = audioop.mul(source_data, 2, self.vol*(1-self.vol_step))
+        overlay_data = audioop.mul(overlay_data, 2, self.vol*self.vol_step)
+
+        return audioop.add(source_data, overlay_data, 2)
+
+    def cleanup(self):
+        self.source.cleanup()
+
+    def vol_change_step(self):
+        if self.vol_step < 1:
+            self.vol_step += 0.1
+
+
+class Player:
+    opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'audioformat': 'flac',
+        'quiet': True,
+        'default_search': 'auto'
+    }
+
+    def __init__(self, voice_client, channel, queue: str=None):
+        self.vc = voice_client
+        self.chan = channel
+        self._queue = queue or []
+        self.source = None
+
+    async def queue(self, song, requester=None):
+        entry = await get_entry(song, self.opts, self.vc.loop)
+        entry["requester"] = requester
+        self._queue.append(entry)
+        await self.chan.send('Added {} to the queue!'.format(entry['title']))
+
+    async def download_next(self):
+        next = self._queue.pop(0)
+
+        await self.chan.send('Next up: {}'.format(next['title']))
+        entry = await get_entry(next['url'], self.opts, self.vc.loop)
+        dl_url = entry['raw_url']
+
+        self._start_time = time.time()
+
+        self.current_song = next
+
+        if self.source is not None:
+            self.source = OverlaySource(self.source, dl_url, vc=self.vc)
+            self.vc.source = self.source
+        else:
+            source = discord.FFmpegPCMAudio(dl_url)
+            self.source = source
+            self.vc.play(source, after=music_after)
+
+    def start(self):
+        self._stop = False
+        self._task = self.vc.loop.create_task(self.process_queue())
+
+    def stop(self):
+        self._stop = True
+        self._task.cancel()
+        self.vc.stop()
+        asyncio.run_coroutine_threadsafe(self.vc.disconnect(), self.vc.loop)
+        self._queue.clear()
+
+    async def process_queue(self):
+        await self.download_next()
+        queue_next = True
+
+        while True:
+            if self._stop:
+                break
+
+            await asyncio.sleep(1)
+            try:
+                if self.current_song['stream']:
+                    # Streams have no duration
+                    continue
+            except AttributeError:
+                # current_song not yet loaded
+                continue
+
+            now = time.time()
+            time_left = self.current_song['duration'] - (now-self._start_time)
+            self.percentage = 1 - (time_left / self.current_song['duration'])
+            if time_left <= 10:
+                if queue_next is False:
+                    continue
+                queue_next = False
+                # Less than 10 seconds left until
+                # the song ends, start the next song.
+                if len(self._queue) > 0:
+                    # There are still songs queued
+
+                    # This data will mix with the data in the previous thread,
+                    # Not sure if this will mess up but we'll see how it goes
+                    await self.download_next()
+
+                else:
+                    await self.chan.send("Queue empty, stopping...")
+                    await asyncio.sleep(time_left)
+                    return self.stop()
+
+                for _ in range(round(time_left)-1):
+                    self.source.vol_change_step()
+                    await asyncio.sleep(1)
+
+            else:
+                queue_next = True
+
+
+class music:
+    def __init__(self, xeili):
+        self.xeili = xeili
+        self.players = {}
+        self.queues = {}
+
+    @commands.command(name='play')
+    async def music_play(self, ctx, *, song: str):
+        start = False
+        if ctx.guild.id not in self.players:
+            vc = await ctx.author.voice.channel.connect(reconnect=True)
+            self.queues[ctx.guild.id] = []
+            self.players[ctx.guild.id] = Player(vc, ctx.channel,
+                                                self.queues[ctx.guild.id])
+            start = True
+
+        await self.players[ctx.guild.id].queue(song, requester=ctx.author)
+
+        if start:
+            self.players[ctx.guild.id].start()
+
+    @commands.command(name='queue')
+    async def music_playlist(self, ctx):
+        await ctx.send('\n'.join(s['name'] for s in self.queues[ctx.guild.id]))
+
+    @commands.command(name="disconnect")
+    async def music_disconnect(self, ctx):
+        self.players[ctx.guild.id].stop()
+        await self.players[ctx.guild.id].vc.disconnect()
+
+    @commands.command(name="song")
+    async def music_current_song(self, ctx):
+        song = self.players[ctx.guild.id].current_song
+        title = song["title"]
+        url = song["url"]
+        req = str(song["requester"])
+        upl = song["uploader"]
+        perc = self.players[ctx.guild.id].percentage
+        prog = "#"*round(perc*10)+"-"*round((1-perc)*10)
+
+        e = discord.Embed(title="Now Playing",
+                          description=title)
+
+        e.add_field(name="Source", value="[Click here!]({})".format(url))
+        e.add_field(name="Progress", value="`[{}]` - {}%".format(
+                prog, round(perc*100)))
+        e.add_field(name="Uploaded by", value=upl, inline=True)
+        e.add_field(name="Requested by", value=req, inline=True)
+
+        await ctx.send(embed=e)
+
+
+def setup(xeili):
+    xeili.add_cog(music(xeili))
