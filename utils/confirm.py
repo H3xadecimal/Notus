daaf9f7caaacd063f3a4e1c0c7a855aeb78f36b6
martmists <martmists@gmail.com>
2017-05-08 18:48:07 +0000

    Update .gitignore

--- utils/confirm.py
+++ utils/confirm.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 
 def is_owner_check(ctx):
-    return str(ctx.message.author.id) in ['ID']
+    return str(ctx.message.author.id) in ['161866631004422144']
 
 
 def instance_owner():
