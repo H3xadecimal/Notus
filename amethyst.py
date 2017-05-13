ac112109d70b10847c58eb632dcccea1cca836bd
Hexadecimal <HexadecimalPython@users.noreply.github.com>
2017-05-13 18:43:05 +0400

    Amethyst update.
    Was unhappy with the old name, Rest in peace Xeili.

---
+++ amethyst.py
@@ -0,0 +1,103 @@
+from discord.ext import commands
+from utils.dataIO import dataIO
+from utils.lookups import Lookups
+import traceback
+import redis
+import argparse
+import json
+
+with open("config.json") as f:
+    config = json.load(f)
+
+redis_host = config.get('AMETHYST_REDIS_HOST') or 'localhost'
+redis_pass = config.get('AMETHYST_REDIS_PASSWORD')
+redis_port = int(config.get('AMETHYST_REDIS_PORT') or 6379)
+redis_db = int(config.get('AMETHYST_REDIS_DB') or 0)
+token = config.get('AMETHYST_TOKEN')
+prefix = config.get('AMETHYST_PREFIX')
+
+# CMD-L Arguments
+parser = argparse.ArgumentParser()
+redis_grp = parser.add_argument_group('redis')
+redis_grp.add_argument('--host', type=str,
+                       help='the Redis host', default=redis_host)
+redis_grp.add_argument('--port', type=int,
+                       help='the Redis port', default=redis_port)
+redis_grp.add_argument('--db', type=int,
+                       help='the Redis database', default=redis_db)
+redis_grp.add_argument('--password', type=str,
+                       help='the Redis password', default=redis_pass)
+args = parser.parse_args()
+
+# Redis Connection Attempt... hopefully works.
+try:
+    redis_conn = redis.StrictRedis(host=args.host,
+                                   port=args.port,
+                                   db=args.db,
+                                   password=args.password)
+except:
+    print('aaaaaaa unable to redis 404')
+    exit(2)
+
+
+class Amethyst(commands.Bot):
+    def __init__(self, command_prefix, args, redis, **options):
+        super().__init__(command_prefix, **options)
+        self.args = args
+        self.redis = redis
+        self.send_command_help = send_cmd_help
+        self.settings = dataIO.load_json('settings')
+        self.blacklist_check = self.loop.create_task(self.blacklist_check())
+
+    async def blacklist_check(self):
+        if 'blacklist' not in self.settings:
+            self.settings['blacklist'] = []
+        else:
+            pass
+
+    async def on_ready(self):
+        self.redis.set('__info__',
+                       'This database is being used by the Amethyst Framework.')
+        print('Ready.')
+        print(self.user.name)
+
+        self.load_extension('modules.core')
+
+    async def on_command_error(self, exception, context):
+        print(exception)
+        if isinstance(exception, commands.errors.CommandNotFound):
+            pass
+        if isinstance(exception, commands.errors.MissingRequiredArgument):
+            await send_cmd_help(context)
+        elif isinstance(exception, commands.errors.CommandInvokeError):
+            exception = exception.original
+            _traceback = traceback.format_tb(exception.__traceback__)
+            _traceback = ''.join(_traceback)
+            error = ('`{0}` in command `{1}`: ```py\n'
+                     'Traceback (most recent call last):\n{2}{0}: {3}\n```')\
+                .format(type(exception).__name__,
+                        context.command.qualified_name,
+                        _traceback, exception)
+            await context.send(error)
+        elif isinstance(exception, commands.errors.CommandNotFound):
+            pass
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.author.id in self.settings['blacklist']:
+            return
+        await self.process_commands(message)
+
+async def send_cmd_help(ctx):
+    if ctx.invoked_subcommand:
+        _help = await ctx.bot.formatter.format_help_for(
+                ctx, ctx.invoked_subcommand)
+    else:
+        _help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
+    for page in _help:
+        await ctx.send(page)
+
+
+amethyst = Amethyst(prefix, args, redis_conn)
+amethyst.run(token)
---
+++ modules/core.py
@@ -0,0 +1,107 @@
+import discord
+from discord.ext import commands
+import importlib
+import inspect
+from utils import confirm
+from utils.dataIO import dataIO
+
+
+class core:
+    def __init__(self, amethyst):
+        self.amethyst = amethyst
+        self.firmware = "Stock Firmware: Compact 0.3"
+        self.settings = dataIO.load_json('settings')
+        self.post_task = self.amethyst.loop.create_task(self.post())
+
+    def __unload(self):
+        self.post_task.cancel()
+
+    async def post(self):
+        if 'modules' not in self.settings:
+            self.settings['modules'] = []
+        else:
+            for module in self.settings['modules']:
+                if module not in list(self.amethyst.extensions):
+                    try:
+                        self.amethyst.load_extension(module)
+                    except:
+                        self.settings['modules'].remove(module)
+                        print("A module blew up... Idk which tho.")
+
+    @commands.command(aliases=['cog'])
+    @confirm.instance_owner()
+    async def module(self, ctx, name: str, argument: str):
+        """Module management."""
+        module_name = 'modules.{0}'.format(name)
+        argumentlist = ["--load", "--unload", "--reload", None]
+        if argument == '--load' or None:
+            if module_name not in list(self.amethyst.extensions):
+                plugin = importlib.import_module(module_name)
+                importlib.reload(plugin)
+                self.amethyst.load_extension(plugin.__name__)
+                self.settings['modules'].append(module_name)
+                await ctx.send('Module loaded.')
+            else:
+                await ctx.send('The module you are trying to load is already loaded.\n'
+                    'Please use the `--reload` argument instead.')
+        if argument == '--unload':
+            if module_name in list(self.amethyst.extensions):
+                plugin = importlib.import_module(module_name)
+                importlib.reload(plugin)
+                self.amethyst.unload_extension(plugin.__name__)
+                self.settings['modules'].remove(module_name)
+                await ctx.send('Module unloaded.')
+            else:
+                await ctx.send(
+                        'The module you are trying to unload could not be found or is not loaded.')
+        if argument == '--reload':
+            if module_name in list(self.amethyst.extensions):
+                plugin = importlib.import_module(module_name)
+                importlib.reload(plugin)
+                self.amethyst.unload_extension(plugin.__name__)
+                self.amethyst.load_extension(plugin.__name__)
+                await ctx.send('Module reloaded.')
+            else:
+                await ctx.send(
+                        'The module you are trying to reload is not loaded.\n'
+                        'Please try the `--reload` argument.')
+        if argument not in argumentlist:
+            await ctx.send(
+                    "The argument you specified is invalid.\n"
+                    "Please check the available arguments using `[prefix]arguments`.")
+
+    @commands.command()
+    async def arguments(self, ctx):
+        """Lists all arguments."""
+        await ctx.send(
+            "Arguments for Modules include: `--load, --unload & --reload`.")
+
+    @commands.command(aliases=['debug'])
+    @confirm.instance_owner()
+    async def eval(self, ctx, *, code: str):
+        env = {
+            "message": ctx.message,
+            "author": ctx.message.author,
+            "channel": ctx.message.channel,
+            "guild": ctx.message.guild,
+            "ctx": ctx,
+            "discord": discord,
+            "amethyst": self.amethyst,
+        }
+
+        output = eval(code, env)
+        if inspect.isawaitable(output):
+            output = await output
+
+        await ctx.send('```py\n{0}\n```'.format(output))
+
+    @commands.command(aliases=['kys'])
+    @confirm.instance_owner()
+    async def shutdown(self, ctx):
+        """Shuts down the bot.... Duh."""
+        await ctx.send("Logging out...")
+        await self.amethyst.logout()
+
+
+def setup(amethyst):
+    amethyst.add_cog(core(amethyst))
