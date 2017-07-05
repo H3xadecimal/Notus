1e4b513a43a1064d6a2f9177d5a16498429bda90
Ovyerus <iamovyerus@gmail.com>
2017-07-05 13:33:13 +1000

    Add new command system

---
+++ utils/command_system.py
@@ -0,0 +1,373 @@
+from typing import Callable, List, Union
+from utils import message_parsing
+import discord
+import inspect
+import re
+import sys
+
+PRETTY_PRINTED_PERMS = {
+    'create_insant_invite': 'Create Instant Invite',
+    'kick_members': 'Kick Members',
+    'ban_members': 'Ban Members',
+    'administrator': 'Administrator',
+    'manage_channels': 'Manage Channels',
+    'manage_guild': 'Manage Server',
+    'add_reactions': 'Add Reactions',
+    'read_messages': 'Read Messages',
+    'send_messages': 'Send Messages',
+    'send_tts_messages': 'Send TTS Messages',
+    'manage_messages': 'Manage Messages',
+    'embed_links': 'Embed Links',
+    'attach_files': 'Attach Files',
+    'read_message_history': 'Read Message History',
+    'mention_everyone': 'Mention Everyone',
+    'external_emojis': 'Use External Emojis',
+    'connect': 'Connect',
+    'speak': 'Speak',
+    'mute_embers': 'Mute Members',
+    'deafen_members': 'Deafen Members',
+    'move_members': 'Move Members',
+    'use_voice_activation': 'Use Voice Activity',
+    'change_nickname': 'Change Nickname',
+    'manage_nicknames': 'Manage Nicknames',
+    'manage_roles': 'Manage Roles',
+    'manage_webhooks': 'Manage Webhooks',
+    'manage_emojis': 'Manage Emojis'
+}
+
+USED_OPTIONS = ['args', 'suffix', 'clean_args', 'clean_suffix', 'cmd']
+
+
+class Context:
+    def __init__(self, msg: discord.Message, amethyst: discord.Client):
+        cleaned = message_parsing.parse_prefixes(msg.content, amethyst.config['AMETHYST_PREFIXES'])
+        self.msg = msg
+        self.cmd = message_parsing.get_cmd(cleaned)
+        self.suffix, self.args = message_parsing.get_args(cleaned)
+
+    async def __send__(self, content, dest, *, embed=None, file=None, files=None):
+        '''Internal send function, not actually ment to be used by anyone.'''
+        if dest == 'channel':
+            return await self.msg.channel.send(content, embed=embed, file=file, files=files)
+        elif dest == 'author':
+            return await self.msg.author.send(content, embed=embed, file=file, files=files)
+        else:
+            raise ValueError('Destination is not `channel` or `author`.')
+
+    async def send(self, content: str=None,
+                   *, dest: str='channel',
+                   embed: discord.Embed=None, file: discord.File=None,
+                   files: List[discord.File]=None):
+        '''Sends a message to the context origin, can either be the channel or author.'''
+        if content is None and not embed and not file and not files:
+            raise TypeError('No content and no attachments.')
+        elif content:
+            # Escape bad mentions
+            content = str(content).replace('@everyone', '@\u200Beveryone').replace('@here', '@\u200Bhere')
+
+        msg = None
+
+        # Splitting messages if they are larger than 2000 chars.
+        # Also properly does codeblocks.
+        # (Could be done nicer but eh)
+        if content and len(content) > 2000:
+            if content.find('```') == -1 or content.find('```', content.find('```') + 3) == -1:
+                await self.__send__(content[:2000], dest, embed=embed, file=file, files=files)
+                await self.send(content[2000:], dest=dest)
+            elif content.find('```', content.find('```') + 3) + 2 < 2000:
+                await self.__send__(content[:content.find('```', content.find('```') + 3) + 3], dest,
+                                    embed=embed, file=file, files=files)
+                await self.send(content[content.find('```', content.find('```') + 3) + 3:], dest=dest)
+            else:
+                start_block = content[content.find('```'):content.find('\n', content.find('```')) + 1]
+
+                if content.find('\n', content.find('```')) == content.rfind('\n', 0, 2000):
+                    split_cont = content[:1996] + '\n```'
+                    content = start_block + content[1996:]
+                else:
+                    split_cont = content[:content.rfind('\n', 0, content.rfind('\n', 0, 2000) + 1)][:1996] + '\n```'
+                    content = start_block + content[len(split_cont) - 4:]
+
+                await self.send(split_cont + content, dest=dest, embed=embed, file=file, files=files)
+        else:
+            msg = await self.__send__(content, dest, embed=embed, file=file, files=files)
+
+        return msg
+
+    def is_dm(self):
+        return isinstance(self.msg.channel, discord.DMChannel)
+
+    def has_permission(self, permission, who='self'):
+        """Check if someone in context has a permission."""
+        if who not in ['self', 'author']:
+            raise ValueError('Invalid value for `who` (must be `self` or `author`).')
+
+        if permission not in PRETTY_PRINTED_PERMS:
+            return False
+
+        if who == 'self':
+            return getattr(self.msg.channel.permissions_for(self.msg.guild.me), permission)
+        elif who == 'author':
+            return getattr(self.msg.channel.permissions_for(self.msg.author), permission)
+
+
+class Command:
+    def __init__(self, func: Callable[..., None],
+                 *, name: str=None, description: str = '',
+                 aliases: list = [], usage: str = '',):
+        self.func = func
+        self.name = name or func.__name__
+        self.description = description or inspect.cleandoc(func.__doc__ or '')
+        self.usage = usage
+        self.aliases = aliases or []
+        self.cls = None
+        self.checks = func._checks if '_checks' in dir(func) else []
+
+    def __repr__(self) -> str:
+        return self.name
+
+    async def run(self, ctx: Context) -> None:
+        if not self.checks:
+            await self.func(self.cls, ctx)
+        else:
+            can_run = True
+
+            for check in self.checks:
+                if inspect.iscoroutinefunction(check):
+                    res = await check(ctx)
+                else:
+                    res = check(ctx)
+
+                if not res:
+                    can_run = False
+                    break
+
+            if can_run:
+                await self.func(self.cls, ctx)
+
+
+class CommandGroup(Command):
+    def __init__(self, func, **attrs):
+        super().__init__(func, **attrs)
+        self.all_commands = {}
+
+    @property
+    def commands(self):
+        return set(self.all_commands.values())
+
+    def add_command(self, cmd):
+        if not isinstance(cmd, Command):
+            raise TypeError("Passed command isn't a Command instance.")
+
+        if cmd.name in self.all_commands:
+            raise AttributeError(f'Command `{cmd.name}` is already registered.')
+
+        self.all_commands[cmd.name] = cmd
+
+        for alias in cmd.aliases:
+            if alias in self.all_commands:
+                raise AttributeError(f'Aliases or command `{cmd.name}` is already registed.')
+
+            self.all_commands[alias] = cmd
+
+    async def run(self, ctx: Context) -> None:
+        if not ctx.args or ctx.args[0] not in self.all_commands:
+            if not self.checks:
+                await self.func(self.cls, ctx)
+            else:
+                can_run = True
+
+                for check in self.checks:
+                    if inspect.iscoroutinefunction(check):
+                        res = await check(ctx)
+                    else:
+                        res = check(ctx)
+
+                    if not res:
+                        can_run = False
+                        break
+
+                if can_run:
+                    await self.func(self.cls, ctx)
+        else:
+            cmd = self.all_commands[ctx.args[0]]
+            ctx.suffix = ctx.suffix.split(' ', 1)[1:]
+            ctx.cmd += ' ' + ctx.args[0]
+
+            if ctx.suffix:
+                ctx.suffix = ctx.suffix[0]
+            else:
+                ctx.suffix = ''
+
+            del ctx.args[0]
+
+            if not self.checks:
+                await cmd.run(ctx)
+            else:
+                # Obey the parent's checks if they have an.
+                can_run = True
+
+                for check in self.checks:
+                    if inspect.iscoroutinefunction(check):
+                        res = await check(ctx)
+                    else:
+                        res = check(ctx)
+
+                    if not res:
+                        can_run = False
+                        break
+
+                if can_run:
+                    await cmd.run(ctx)
+
+    def command(self, **attrs):
+        def decorator(func):
+            res = command(**attrs)(func)
+
+            self.add_command(res)
+            return res
+
+        return decorator
+
+
+class CommandHolder:
+    def __init__(self, amethyst):
+        self.commands = {}
+        self.aliases = {}
+        self.modules = {}
+        self.amethyst = amethyst
+
+    def __len__(self):
+        return len(self.commands)
+
+    def __contains__(self, x: str) -> bool:
+        return x in self.commands
+
+    def load_module(self, module_name: str) -> None:
+        if module_name in self.modules:
+            raise Exception(f'Module `{module_name}` is already loaded.')
+
+        module = getattr(__import__(module_name), module_name.split('.')[-1])
+
+        try:
+            module.setup
+        except AttributeError:
+            del sys.modules[module_name]
+            raise Exception('Module does not have a `setup` function.')
+
+        module = module.setup(self.amethyst)
+        cmds = [x for x in dir(module) if not re.match('__?.*(?:__)?', x) and isinstance(getattr(module, x), Command)]
+        loaded_cmds = []
+        loaded_aliases = []
+
+        if not cmds:
+            del sys.modules[module_name]
+            raise ValueError('Module is empty.')
+
+        for cmd in cmds:
+            cmd = getattr(module, cmd)
+
+            if not isinstance(cmd, Command):
+                continue
+
+            cmd.cls = module
+            self.commands[cmd.name] = cmd
+
+            for alias in cmd.aliases:
+                self.aliases[alias] = self.commands[cmd.name]
+                loaded_aliases.append(alias)
+
+            loaded_cmds.append(cmd.name)
+
+        self.modules[module_name] = loaded_cmds + loaded_aliases
+
+    def reload_module(self, module_name: str) -> None:
+        if module_name not in self.modules:
+            self.load_module(module_name)
+            return
+
+        self.unload_module(module_name)
+        self.load_module(module_name)
+
+    def unload_module(self, module_name: str) -> None:
+        if module_name not in self.modules:
+            raise Exception(f'Module `{module_name}` is not loaded.')
+
+        for cmd in self.modules[module_name]:
+            if cmd in self.aliases:
+                del self.aliases[cmd]
+            elif cmd in self.commands:
+                del self.commands[cmd]
+
+        del self.modules[module_name]
+        del sys.modules[module_name]
+
+    def get_command(self, cmd_name: str) -> Union[Command, None]:
+        return self.aliases[cmd_name] if cmd_name in self.aliases else\
+               self.commands[cmd_name] if cmd_name in self.commands else None
+
+    async def run_command(self, ctx: Context) -> None:
+        cmd = self.get_command(ctx.cmd)
+
+        if not cmd:
+            return
+
+        await cmd.run(ctx)
+
+    @property
+    def all_commands(self) -> List[str]:
+        return sorted(self.commands.keys())
+
+    @property
+    def all_aliases(self) -> List[str]:
+        return sorted(self.aliases.keys())
+
+    @property
+    def all_modules(self) -> List[str]:
+        return sorted(self.modules.keys())
+
+
+# Command conversion decorator
+def command(**attrs):
+    def decorator(func):
+        if isinstance(func, Command):
+            raise TypeError('Function is already a command.')
+
+        if not inspect.iscoroutinefunction(func):
+            raise TypeError("Command function isn't a coroutine.")
+
+        return Command(func, **attrs)
+
+    return decorator
+
+
+# Command group conversion decorator
+def group(**attrs):
+    def decorator(func):
+        if isinstance(func, CommandGroup):
+            raise TypeError('Function is already a command group.')
+        elif isinstance(func, Command):
+            raise TypeError('Function is already a command.')
+
+        if not inspect.iscoroutinefunction(func):
+            raise TypeError("Command function isn't a coroutine.")
+
+        return CommandGroup(func, **attrs)
+
+    return decorator
+
+
+# Command checker convertor for decorators
+def check(checker):
+    def decorator(func):
+        if isinstance(func, Command):
+            func.checks.append(checker)
+        else:
+            if '_checks' not in dir(func):
+                func._checks = []
+
+            func._checks.append(checker)
+
+        return func
+
+    return decorator
