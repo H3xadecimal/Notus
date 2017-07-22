72c7910bfd365ecd69f22d3cb23ce4691937f7df
Ovyerus <iamovyerus@gmail.com>
2017-07-22 16:00:31 +1000

    @instance_owner now makes the command hidden

--- utils/confirm.py
+++ utils/confirm.py
@@ -6,7 +6,7 @@ def instance_owner():
     def checker(ctx):
         return str(ctx.msg.author.id) in __main__.amethyst.owners or ctx.msg.author.id == 99742488666845184
 
-    return check(checker)
+    return check(checker, True)
 
 
 def instance_guild():
