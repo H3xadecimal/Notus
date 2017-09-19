69e6db7a4a28ff1f50bd4f12f550a2b65f05eb38
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-19 17:33:31 +1000

    Remove obsolete TODO and add version

--- utils/dusk/__init__.py
+++ utils/dusk/__init__.py
@@ -1,11 +1,11 @@
 """
 'Dusk' command system for Amethyst.
 Based loosely (heh) off of discord.py's ext command system.
-
-TODO: refactor arg parsing probably.
 """
 
 from .context import Context  # NOQA
 from .command import *  # NOQA
 from .command_holder import CommandHolder  # NOQA
 from .constants import *  # NOQA
+
+__version__ = "1.0.0"
