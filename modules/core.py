d5c3de66dd29aa280be7e7401153edbc9ba481ff
Ovyerus <iamovyerus@gmail.com>
2020-01-15 16:27:52 +1100

    refactor(modules/core): properly move to ext

--- modules/core.py
+++ modules/core.py
@@ -1,120 +1,94 @@
 import textwrap
 import time
 import traceback
+from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands
 
 from utils import check
 
+if TYPE_CHECKING:
+    from notus import Notus
 
-class Core:
-    def __init__(self, notus):
+
+class Core(commands.Cog):
+    def __init__(self, notus: Notus):
         self.notus = notus
-        self.db = notus.db
         self._eval = {}
 
         self.post_load()
 
     @property
     def settings(self):
-        return self.db["settings"]
+        return self.notus.db["settings"]
 
     def post_load(self):
         if "modules" not in self.settings:
             self.settings["modules"] = []
 
         for module in self.settings["modules"]:
-            if module not in list(self.notus.extensions):
+            if module not in self.notus.extensions:
                 try:
                     self.notus.load_extension(module)
                 except Exception as e:
                     self.settings["modules"].remove(module)
 
-                    print(f"Module `{module}` blew up.")
+                    print(f"Extension `{module}` blew up.")
                     print("".join(traceback.format_tb(e.__traceback__)))
 
-        if "owners" not in self.settings:
-            self.settings["owners"] = []
+    @commands.group(aliases=["cog"])
+    @check.owner()  # TODO: does this apply to whole group?
+    async def module(self, ctx: commands.Context):
+        """Module management"""
+        await ctx.send_help(ctx.command)
 
-        if self.notus.owner not in self.settings["owners"]:
-            self.settings["owners"].append(self.notus.owner)
+    @module.command("load")
+    async def module_load(self, ctx: commands.Context, *, module: str):
+        module = "modules." + module.lower()
 
-        self.notus.owners = self.settings["owners"]
+        if module not in self.notus.extensions:
+            self.notus.load_extension(module)
+            self.settings["modules"]
 
-    @commands.command()
-    async def help(self, ctx):
-        """Show help for all the commands."""
-        if not ctx.args:
-            try:
-                await self.notus.send_command_help(ctx)
-            except discord.Forbidden:
-                await ctx.send(
-                    "Cannot send the help to you. Perhaps you have DMs blocked?"
-                )
+            await ctx.send("Module loaded.")
         else:
-            ctx.cmd = ctx.suffix
-            await self.notus.send_command_help(ctx)
-
-    # At the time of rewriting this, I am unaware if importlib is still required to load/unload/reload
-    # modules on the latest version of discord.py as the rewrite was in beta during this code and is now on release version.
-    # CC @Ovyerus to test.
-
-    @commands.command(aliases=["cog"])
-    @check.instance_owner()
-    async def module(self, ctx, *, argument: str = None, module: str):
-        """Module management."""
-
-        argument_list = ["load", "unload", "reload"]
-        module_name = "modules." + module.lower()
-
-        if argument == "load" or argument is None:
-            if module_name not in list(self.notus.extensions):
-                self.notus.load_extension(module_name)
-                self.settings["modules"].append(module_name)
-                await ctx.send("Module loaded.")
-            else:
-                await ctx.send(
-                    "The module you are trying to load is already loaded.\n"
-                    "Please use the `reload` argument instead."
-                )
-        elif argument == "unload":
-            if module_name in list(self.notus.extensions):
-                self.notus.unload_extension(module_name)
-                self.settings["modules"].remove(module_name)
-                await ctx.send("Module unloaded.")
-            else:
-                await ctx.send(
-                    "The module you are trying to unload "
-                    "could not be found or is not loaded."
-                )
-        elif argument == "--reload":
-            if module_name in list(self.notus.extensions):
-                self.notus.reload_extension(module_name)
-                await ctx.send("Module reloaded.")
-            else:
-                await ctx.send(
-                    "The module you are trying to reload is not loaded.\n"
-                    "Please try the `load` argument."
-                )
-        elif argument not in argument_list:
             await ctx.send(
-                "The argument you specified is invalid.\n"
-                "Please check the available arguments using"
-                " `[prefix]arguments`."
+                "That module is already loaded.\n"
+                "Try using `module reload <name>` instead."
             )
 
-    @command()
-    @check.instance_owner()
-    async def arguments(self, ctx):
-        """Lists all arguments."""
-        await ctx.send("Arguments for modules include: `load, unload & reload`.")
+    @module.command("unload")
+    async def module_unload(self, ctx: commands.Context, *, module: str):
+        module = "modules." + module.lower()
 
-    @command(aliases=["kys"])
-    @check.instance_owner()
-    async def shutdown(self, ctx):
-        """Shuts down the bot.... Duh."""
-        await ctx.send("Logging out...")
+        if module in self.notus.extensions:
+            self.notus.unload_extension(module)
+            self.settings["modules"].removbe(module)
+
+            await ctx.send("Module unloaded.")
+        else:
+            await ctx.send("That module is not currently loaded.")
+
+    @module.command("reload")
+    async def module_reload(self, ctx: commands.Context, *, module: str):
+        module = "modules." + module.lower()
+
+        if module in self.notus.extensions:
+            self.notus.reload_extension(module)
+
+            await ctx.send("Module reloaded.")
+        else:
+            await ctx.send(
+                "That module isn't currently loaded.\n"
+                "Try using `module load <name>` instead."
+            )
+
+    @commands.command(aliases=["kys"])
+    @check.owner()
+    async def shutdown(self, ctx: commands.Context):
+        """Shuts down the bot... duh"""
+        await ctx.send("Cya")
         await self.notus.logout()
 
     # Now this piece of shit code is partially broken.
@@ -123,85 +97,88 @@ class Core:
     # Temporarily disabled until fixed.
     # Also leaving that one to @Ovyerus because my last 6 attempts at fixing it failed.
 
-    @command(aliases=["debug"], usage="<code>")
+    @commands.command(aliases=["debug"])
     @check.instance_owner()
-    async def eval(self, ctx):
-        await ctx.send("This command is currently disabled.")
+    async def eval(self, ctx: commands.Context, code: commands.Greedy[str]):
+        if self._eval.get("env") is None:
+            self._eval["env"] = {}
+        if self._eval.get("count") is None:
+            self._eval["count"] = 0
 
+        self._eval["env"].update(
+            {
+                "ctx": ctx,
+                "message": ctx.message,
+                "channel": ctx.message.channel,
+                "guild": ctx.message.guild,
+                "server": ctx.message.guild,
+                "author": ctx.message.author,
+            }
+        )
 
-#        if self._eval.get('env') is None:
-#            self._eval['env'] = {}
-#        if self._eval.get('count') is None:
-#            self._eval['count'] = 0
-#
-#        self._eval['env'].update({
-#            'lookups': self.lookups,
-#            'ctx': ctx,
-#            'message': ctx.msg,
-#            'channel': ctx.msg.channel,
-#            'guild': ctx.msg.guild,
-#            'server': ctx.msg.guild,
-#            'author': ctx.msg.author
-#        })
-#
-# let's make this safe to work with
-#        code = ctx.suffix.replace('```py\n', '').replace('```', '').replace('`', '')
-#        _code = "async def func(self):\n  try:\n{}\n  finally:\n    self._eval['env'].update(locals())"\
-#                .format(textwrap.indent(code, '    '))
-#        before = time.monotonic()
-#
-# noinspection PyBroadException
-#        try:
-#            exec(_code, self._eval['env'])
-#
-#            func = self._eval['env']['func']
-#            output = await func(self)
-#
-#            if output is not None:
-#                output = repr(output)
-#        except Exception as e:
-#            output = '{}: {}'.format(type(e).__name__, e)
-#
-#        after = time.monotonic()
-#        self._eval['count'] += 1
-#        count = self._eval['count']
-#        code = code.split('\n')
-#
-#        if len(code) == 1:
-#            _in = 'In [{}]: {}'.format(count, code[0])
-#        else:
-#            _first_line = code[0]
-#            _rest = code[1:]
-#            _rest = '\n'.join(_rest)
-#            _countlen = len(str(count)) + 2
-#            _rest = textwrap.indent(_rest, '...: ')
-#            _rest = textwrap.indent(_rest, ' ' * _countlen)
-#            _in = 'In [{}]: {}\n{}'.format(count, _first_line, _rest)
-#
-#        message = '```py\n{}'.format(_in)
-#        ms = int(round((after - before) * 1000))
-#
-#        if output is not None:
-#            message += '\nOut[{}]: {}'.format(count, output)
-#
-#        if ms > 100:  # noticeable delay
-#            message += '\n# {} ms\n```'.format(ms)
-#        else:
-#            message += '\n```'
-#
-#        try:
-#            if ctx.msg.author.id == self.notus.user.id:
-#                await ctx.msg.edit(content=message)
-#            else:
-#                await ctx.send(message)
-#        except discord.HTTPException:
-#            await ctx.msg.channel.trigger_typing()
-#            await ctx.send('Output was too big to be printed.')
+        # let's make this safe to work with
+        code = ctx.suffix.replace("```py\n", "").replace("```", "").replace("`", "")
+        to_eval = textwrap.dedent(
+            f"""
+            async def func(self):
+              try:
+                {textwrap.index(code, '    ')}
+              finally:
+                self._eval['env'].update(locals())
+        """
+        ).strip()
+        before = time.monotonic()
+
+        try:
+            exec(to_eval, self._eval["env"])
+
+            func = self._eval["env"]["func"]
+            output = await func(self)
+
+            if output is not None:
+                output = repr(output)
+        except Exception as e:
+            output = f"{type(e).__name__}: {e}"
+
+        after = time.monotonic()
+        self._eval["count"] += 1
+        count = self._eval["count"]
+        code = code.split("\n")
+
+        if len(code) == 1:
+            in_ = f"In [{count}]: {code[0]}"
+        else:
+            first = code[0]
+            rest = code[1:]
+            rest = "\n".join(rest)
+            count_len = len(str(count)) + 2
+            rest = textwrap.indent(textwrap.indent(rest, "...: "), " " * count_len)
+
+            in_ = f"In [{count}]: {first}\n{rest}"
+
+        message = f"```py\n{in_}"
+        ms = round((after - before) * 1000)
+
+        if output is not None:
+            message += f"\nOut[{count}]: {output}"
+
+        if ms > 100:  # noticeable delay
+            message += f"\n# {ms} ms\n```".format(ms)
+        else:
+            message += "\n```"
+
+        try:
+            if ctx.msg.author.id == self.notus.user.id:
+                await ctx.msg.edit(content=message)
+            else:
+                await ctx.send(message)
+        except discord.HTTPException:
+            await ctx.msg.channel.trigger_typing()
+            await ctx.send("Output was too big to be printed.")
+
 
 # Eval code provided by Pandentia over at Thessia.
 # More of his work here: https://github.com/Pandentia
-
-# Sidenote, uncommenting all of this is gonna be fun, goodluck Ovy.
 
 
 def setup(notus):
