2574552efdca9322a3579cc814cc0e268688ce6d
Ovyerus <iamovyerus@gmail.com>
2020-01-15 18:05:24 +1100

    feat(check): add permission checkers

--- utils/check.py
+++ utils/check.py
@@ -1,44 +1,73 @@
 import discord
-from discord.ext.commands import check
+from discord.ext.commands import Context, check
 
 
 def owner():
-    def checker(ctx):
-        return ctx.message.author.id in ctx.bot.owners
+    """Check if caller is a bot owner"""
+
+    def checker(ctx: Context):
+        return ctx.author.id in ctx.bot.owners
 
     return check(checker)
 
 
 def guild():
-    def checker(ctx):
+    """Check if called in a guild"""
+
+    def checker(ctx: Context):
         return not ctx.is_dm()
 
     return check(checker)
 
 
 def roles(*roles: int):
-    def checker(ctx):
-        return not ctx.is_dm() and len(
-            [x for x in ctx.message.author.roles if x.id in roles]
-        ) == len(roles)
+    """Check if caller has all given roles"""
+
+    def checker(ctx: Context):
+        return not ctx.is_dm() and all([x.id in roles for x in ctx.author.roles])
 
     return check(checker)
 
 
 def named_roles(*roles: str):
-    def checker(ctx):
-        return not ctx.is_dm() and all(
-            [x.name in roles for x in ctx.message.author.roles]
-        )
+    """Check if caller has all given roles, checking by name"""
+
+    def checker(ctx: Context):
+        return not ctx.is_dm() and all([x.name in roles for x in ctx.author.roles])
 
     return check(checker)
 
 
 def nsfw():
-    def checker(ctx):
-        return (
-            isinstance(ctx.message.channel, discord.TextChannel)
-            and ctx.message.channel.is_nsfw()
-        )
+    """Check if called in a NSFW channel"""
+
+    def checker(ctx: Context):
+        return isinstance(ctx.channel, discord.TextChannel) and ctx.channel.is_nsfw()
 
     return check(checker)
+
+
+class permissions:
+    @staticmethod
+    def author(permissions: discord.Permissions):
+        """Check if caller has required permissions"""
+
+        def checker(ctx: Context):
+            return (
+                isinstance(ctx.author, discord.Member)
+                and ctx.author.permissions_in(ctx.channel) >= permissions
+            )
+
+        return check(checker)
+
+    @staticmethod
+    def me(permissions: discord.Permissions):
+        """Check if bot has required permissions"""
+
+        def checker(ctx: Context):
+            return (
+                isinstance(ctx.me, discord.Member)
+                and ctx.me.permissions_in(ctx.channel) >= permissions
+            )
+
+        return check(checker)
